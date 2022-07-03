e9b9c75714b78d7f20595973bcac2f56199ae1f2
AbdElAziz Moftah <vb.promax@gmail.com>
2022-07-03 20:19:48 +0200

    Update main.py

--- tibcogram/source/main.py
+++ tibcogram/source/main.py
@@ -1,32 +1,32 @@
+import os
 import flask
-from math import sqrt
 
+# create flask app
 app = flask.Flask(__name__)
 
-
+# create http template
 template = '''
 <!DOCTYPE html>
 <html lang="en">
 
     <head>
         <meta charset="UTF-8">
-        <title>INSTA TIBCO</title>
+        <title>INSTA - TIBCO</title>
     </head>
 
     <body>
-        <h1>Welcome to instagram (tibco eddition)...</h1>
+        <h1>Welcome to instagram (tibco eddition).</h1>
     </body>
 
 </html>
 '''
 
 
+# main route
 @app.route("/")
 def index():
-    # msg = 'wall is empty?'
-    # return template.replace('@_@', msg)
-    # <img src="" alt="feed">
     return template
 
 
+# run app
 app.run(host='0.0.0.0', port=80)
